2feb865e37ecc60ebdaabfdbc8f9f096882dcee4
gothaw <rad.soltan@gmail.com>
2019-09-10 23:52:01 +0100

    Added enquire.js library.

--- src/js/modules/_navbar.js
+++ src/js/modules/_navbar.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+import enquire from "../lib/enquire.js"
+
+(function () {
+
+
+    const addBackgroundColourNavBar = () => {
+        enquire.register('screen and (max-width: 991px)', {
+            match: function () {
+                console.log("Match");
+            },
+            unmatch: function () {
+                console.log("Unmatch");
+            }
+        });
+    };
+
+    const init = () => {
+        addBackgroundColourNavBar();
+    };
+
+    window.addEventListener('load',init)
+})();
