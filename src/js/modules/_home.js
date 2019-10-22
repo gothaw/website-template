579853495742bbf45bf091b1a47c38ead81d1879
gothaw <rad.soltan@gmail.com>
2019-10-23 00:03:40 +0100

    Refactor of the carousel javascript coding + adding documentation.

--- src/js/modules/_home.js
+++ src/js/modules/_home.js
@@ -4,48 +4,62 @@
     const carouselItems = document.querySelectorAll(".carousel__item");
     const carouselControls = document.querySelectorAll(".intro__carousel-controls i");
 
-    let indexOfCarouselItemShown = 0;
+    // Index of the Carousel Item/Carousel Control that is currently shown/highlighted
+    let currentIndex = 0;
 
+    /**
+     * @name        getChildIndexInParent
+     * @param       element
+     * @desc        Function gets index of the element with respect to its parent.
+     * @returns     {int}
+     */
     const getChildIndexInParent = (element) => {
         return Array.from(element.parentNode.children).indexOf(element);
     };
 
-    const animateCarouselControls = (indexOfCarouselControl) => {
-
+    /**
+     * @name        animateCarouselControls
+     * @param       indexOfSelectedControl {int} index of the empty circle that is selected by user
+     * @param       indexOfHighlightedControl {int} index of the circle that is currently selected i.e. solid circle
+     * @desc        Animates carouser controls by adding and deleting relevant font awesome classes to create empty and solid circles.
+     */
+    const animateCarouselControls = (indexOfSelectedControl, indexOfHighlightedControl) => {
+        carouselControls[indexOfHighlightedControl].classList.remove("fas");
+        carouselControls[indexOfHighlightedControl].classList.add("far");
+        carouselControls[indexOfSelectedControl].classList.remove("far");
+        carouselControls[indexOfSelectedControl].classList.add("fas");
     };
 
-    const checkIfNotShownItem = (index) => {
-        if (index !== indexOfCarouselItemShown) {
-            return index;
-        } else {
-            return false;
-        }
+    /**
+     * @name        showCarouselItem
+     * @param       indexOfSelectedItem {int} index of the carousel item selected by user
+     * @param       indexOfCurrentItem {int} index of the carousel item that is currently shown to user
+     * @desc        Switches between carousel items by changing left property of the carousel DOM element.
+     */
+    const showCarouselItem = (indexOfSelectedItem, indexOfCurrentItem) => {
+        carouselItems[indexOfCurrentItem].classList.remove("carousel__item--selected");
+        carouselItems[indexOfSelectedItem].classList.add("carousel__item--selected");
+        carousel.style.left = `-${indexOfSelectedItem * 100}%`;
     };
 
-    const showCarouselItem = (indexOfCarouselItem) => {
-        if (indexOfCarouselItem !== indexOfCarouselItemShown) {
-            carouselItems[indexOfCarouselItemShown].classList.remove("carousel__item--selected");
-            carouselItems[indexOfCarouselItem].classList.add("carousel__item--selected");
-            carouselControls[indexOfCarouselItemShown].classList.remove("fas");
-            carouselControls[indexOfCarouselItemShown].classList.add("far");
-            carouselControls[indexOfCarouselItem].classList.remove("far");
-            carouselControls[indexOfCarouselItem].classList.add("fas");
-            indexOfCarouselItemShown = indexOfCarouselItem;
-            carousel.style.left = `-${indexOfCarouselItem * 100}%`;
-        }
-    };
-
-    const bindCarouselButtons = () => {
+    /**
+     * @name        animateCarousel
+     * @desc        Animates carousel in the introduction section on the home page. It invokes showCarouselItem and animateCarouselControls functions.
+     *              The carousel is animated when user clicks on the carousel controls - circles below text area.
+     */
+    const animateCarousel = () => {
         for (let control of carouselControls) {
             control.addEventListener("click", function () {
                 let indexOfSelectedItem = getChildIndexInParent(control);
-
+                showCarouselItem(indexOfSelectedItem, currentIndex);
+                animateCarouselControls(indexOfSelectedItem, currentIndex);
+                currentIndex = indexOfSelectedItem;
             })
         }
     };
 
     const init = () => {
-        bindCarouselButtons();
+        animateCarousel();
     };
 
     window.addEventListener("load", init);
