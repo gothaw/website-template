3f8e4d23700af59cdc5bd66486e27ff9ba83bd46
gothaw <rad.soltan@gmail.com>
2019-10-22 23:41:39 +0100

    Added coding for home intro carousel.

--- src/js/modules/_home.js
+++ src/js/modules/_home.js
@@ -1,20 +1,45 @@
 (function () {
     // variables
-    const carouselControls = document.querySelectorAll(".intro__carousel-controls i");
+    const carousel = document.querySelector(".intro__carousel");
     const carouselItems = document.querySelectorAll(".carousel__item");
+    const carouselControls = document.querySelectorAll(".intro__carousel-controls i");
 
-    let selectedCarouselItemIndex = 0;
+    let indexOfCarouselItemShown = 0;
 
     const getChildIndexInParent = (element) => {
         return Array.from(element.parentNode.children).indexOf(element);
     };
 
+    const animateCarouselControls = (indexOfCarouselControl) => {
+
+    };
+
+    const checkIfNotShownItem = (index) => {
+        if (index !== indexOfCarouselItemShown) {
+            return index;
+        } else {
+            return false;
+        }
+    };
+
+    const showCarouselItem = (indexOfCarouselItem) => {
+        if (indexOfCarouselItem !== indexOfCarouselItemShown) {
+            carouselItems[indexOfCarouselItemShown].classList.remove("carousel__item--selected");
+            carouselItems[indexOfCarouselItem].classList.add("carousel__item--selected");
+            carouselControls[indexOfCarouselItemShown].classList.remove("fas");
+            carouselControls[indexOfCarouselItemShown].classList.add("far");
+            carouselControls[indexOfCarouselItem].classList.remove("far");
+            carouselControls[indexOfCarouselItem].classList.add("fas");
+            indexOfCarouselItemShown = indexOfCarouselItem;
+            carousel.style.left = `-${indexOfCarouselItem * 100}%`;
+        }
+    };
+
     const bindCarouselButtons = () => {
         for (let control of carouselControls) {
             control.addEventListener("click", function () {
-                let index = getChildIndexInParent(control);
-                carouselItems[selectedCarouselItemIndex].classList.remove("carousel__item--selected");
-                carouselItems[index].classList.add("carousel__item--selected");
+                let indexOfSelectedItem = getChildIndexInParent(control);
+
             })
         }
     };
