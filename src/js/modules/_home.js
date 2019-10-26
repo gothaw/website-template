1754ccaa315acda88ba99f0233e9cb3dc0cab820
gothaw <rad.soltan@gmail.com>
2019-10-26 23:27:17 +0100

    Added JS for the video controls for home services section. Styling class refactoring for home services.

--- src/js/modules/_home.js
+++ src/js/modules/_home.js
@@ -3,9 +3,15 @@
     const carousel = document.querySelector(".intro__carousel");
     const carouselItems = document.querySelectorAll(".carousel__item");
     const carouselControls = document.querySelectorAll(".intro__carousel-controls i");
+    const homeServices = document.querySelector(".home-services");
+    const servicesVideo = document.querySelector(".home-services__video");
+    const videoControls = homeServices.querySelectorAll(".video-controls__control[data-control]");
+    const playAndPauseIcon = homeServices.querySelector(".video-controls__control[data-control=play-pause] .fas");
 
     // Index of the Carousel Item/Carousel Control that is currently shown/highlighted
     let currentIndex = 0;
+
+    let servicesVideoPlaying = false;
 
     /**
      * @name        getChildIndexInParent
@@ -58,9 +64,59 @@
         }
     };
 
+    /**
+     * @name            playAndPauseVideo
+     * @desc            Plays and pauses services video.
+     */
+    const playAndPauseVideo = () => {
+        if (!servicesVideoPlaying) {
+            playAndPauseIcon.classList.remove("fa-play");
+            playAndPauseIcon.classList.add("fa-pause");
+            servicesVideo.play();
+        } else {
+            playAndPauseIcon.classList.remove("fa-pause");
+            playAndPauseIcon.classList.add("fa-play");
+            servicesVideo.pause();
+        }
+        servicesVideoPlaying = !servicesVideoPlaying;
+    };
+
+    /**
+     * @name            stopVideo
+     * @desc            Stops services video.
+     */
+    const stopVideo = () => {
+        playAndPauseIcon.classList.remove("fa-pause");
+        playAndPauseIcon.classList.add("fa-play");
+        servicesVideo.pause();
+        servicesVideo.currentTime = 0;
+        servicesVideoPlaying = false;
+    };
+
+    /**
+     * @name            bindVideoControls
+     * @desc            Function binds services video controls using switch statement.
+     */
+    const bindVideoControls = () => {
+        for (let control of videoControls) {
+            control.addEventListener("click", function () {
+                const controlName = control.getAttribute("data-control");
+                switch (controlName) {
+                    case 'play-pause':
+                        playAndPauseVideo();
+                        break;
+                    case 'stop':
+                        stopVideo();
+                        break;
+                }
+            })
+        }
+    };
+
 
     const init = () => {
         animateCarousel();
+        bindVideoControls();
     };
 
     window.addEventListener("load", init);
