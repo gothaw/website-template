70d4f24aa0fb8da93f0b55da5baa43d630005a1a
gothaw <rad.soltan@gmail.com>
2019-11-03 15:07:17 +0000

    Ability to change main image and added generic classes for lightbox selectors.

--- src/js/modules/_lightbox.js
+++ src/js/modules/_lightbox.js
@@ -2,12 +2,15 @@ import {getChildIndexInParent} from "../modules/_utilities.js";
 import $ from "../lib/jquery-3.4.1.js"
 
 (function () {
-    const lightbox = document.querySelector(".home-services__lightbox");
+    const lightbox = document.querySelector(".lightbox");
+    const galleryItems = document.querySelectorAll(".gallery__item");
     const lightboxImage = document.querySelector(".lightbox__img");
     const lightboxImageWrapper = document.querySelector(".lightbox__img-wrapper");
     const lightboxDescriptions = document.querySelectorAll(".lightbox__description");
-    const galleryItems = document.querySelectorAll(".home-services__img-wrapper");
+    const lightboxArrowLeft = document.querySelector(".lightbox__arrow--left");
+    const lightboxArrowRight = document.querySelector(".lightbox__arrow--right");
 
+    let lightboxIndex = 0;
 
     const setImageDescription = (index) => {
         for (let description of lightboxDescriptions) {
@@ -16,8 +19,15 @@ import $ from "../lib/jquery-3.4.1.js"
         lightboxDescriptions[index].classList.add("lightbox__description--show");
     };
 
-    const setMainLightBoxImage = () => {
-
+    const setMainLightBoxImage = (e) => {
+        const target = e.target;
+        const targetGalleryItem = target.closest(".gallery__item");
+        const mainImage = targetGalleryItem.querySelector("picture");
+        if(lightboxImageWrapper.children.length > 2){
+            lightboxImageWrapper.removeChild(lightboxImageWrapper.firstChild);
+        }
+        console.log(lightboxImageWrapper.children.length);
+        lightboxImageWrapper.prepend(mainImage.cloneNode(true));
     };
 
     const showLightBox = () => {
@@ -29,22 +39,30 @@ import $ from "../lib/jquery-3.4.1.js"
             e.stopPropagation();
         });
         for (let description of lightboxDescriptions) {
-            description.addEventListener("click",function (e) {
+            description.addEventListener("click", function (e) {
                 e.stopPropagation();
             })
         }
         lightbox.addEventListener("click", function (e) {
-            $(lightbox).hide();
+            $(lightbox).slideUp("fast");
         })
     };
 
+    const bindLightboxArrows = () => {
+        lightboxArrowLeft.addEventListener("click", function () {
+            lightboxIndex === 0 ? lightboxIndex = galleryItems.length - 1 : lightboxIndex--;
+        });
+        lightboxArrowRight.addEventListener("click", function () {
+            lightboxIndex === galleryItems.length - 1 ? lightboxIndex = 0 : lightboxIndex++;
+        });
+    };
 
     const bindGalleryItems = () => {
         for (let item of galleryItems) {
-            item.addEventListener("click", function () {
-                const itemIndex = getChildIndexInParent(item);
-                setMainLightBoxImage();
-                setImageDescription(itemIndex);
+            item.addEventListener("click", function (e) {
+                lightboxIndex = getChildIndexInParent(item);
+                setMainLightBoxImage(e);
+                setImageDescription(lightboxIndex);
                 showLightBox();
             });
         }
@@ -53,6 +71,7 @@ import $ from "../lib/jquery-3.4.1.js"
     const init = () => {
         bindGalleryItems();
         hideLightBox();
+        bindLightboxArrows();
     };
 
     window.addEventListener("load", init);
