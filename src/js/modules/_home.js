83edfcde09930364876ff730ce119597a45ad460
gothaw <rad.soltan@gmail.com>
2019-11-12 23:46:38 +0000

    Documentation for counter animation functions, added functionality to animate counter in thousands.

--- src/js/modules/_home.js
+++ src/js/modules/_home.js
@@ -16,10 +16,10 @@ import {getChildIndexInParent} from "../modules/_utilities.js";
     const milestones = document.querySelector(".milestones__inner");
     const milestoneCounters = milestones.querySelectorAll(".stat__count");
     const milestoneCountersValues = {
-        clients: 43,
-        projects: 55,
-        videos: 312,
-        photos: 4.2
+        clients: 25,
+        projects: 32,
+        videos: 213,
+        photos: 1322
     };
 
     // Index of the Carousel Item/Carousel Control that is currently shown/highlighted
@@ -134,33 +134,62 @@ import {getChildIndexInParent} from "../modules/_utilities.js";
         }, 500);
     };
 
+    /**
+     * @name            checkIfMilestonesAreVisible
+     * @desc            Function checks if company milestones are visible in the viewport.
+     *                  If they are visible, animateMilestoneCounter function is invoked for each counter with a limit that counter counts to.
+     */
     const checkIfMilestonesAreVisible = () => {
         setTimeout(function () {
             if (milestones.getBoundingClientRect().top < window.innerHeight) {
-                animateMilestoneCounters(milestoneCounters[0], milestoneCountersValues.clients);
-                animateMilestoneCounters(milestoneCounters[1], milestoneCountersValues.projects);
-                animateMilestoneCounters(milestoneCounters[2], milestoneCountersValues.videos);
-                animateMilestoneCounters(milestoneCounters[3], milestoneCountersValues.photos);
+                animateMilestoneCounter(milestoneCounters[0], milestoneCountersValues.clients);
+                animateMilestoneCounter(milestoneCounters[1], milestoneCountersValues.projects);
+                animateMilestoneCounter(milestoneCounters[2], milestoneCountersValues.videos);
+                animateMilestoneCounter(milestoneCounters[3], milestoneCountersValues.photos);
             } else {
                 checkIfMilestonesAreVisible();
             }
         }, 200)
     };
 
-    const animateMilestoneCounters = (counter, limit) => {
+    /**
+     * @name            animateMilestoneCounter
+     * @param           counter {Element} DOM element where counting animation takes place
+     * @param           limit {int} limit that counter reaches after animation is finished
+     * @desc            Function animates counter using setTimeout and recursion by modifying innerHTML of counter DOM element.
+     *                  If limit is below 1000, it animates it from 0 to limit by incrementing by one.
+     *                  Alternatively, if limit is above 1000 it animates from 0 to adjusted limit, which displayed as, for example, 1.23k and incrementing by 0.01.
+     */
+    const animateMilestoneCounter = (counter, limit) => {
+        let increaseCount;
         let i = 1;
-        const increaseCount = () => {
-            setTimeout(function () {
-                counter.innerHTML = i.toString();
-                if (i < limit) {
-                    i++;
-                    increaseCount();
-                }
-            }, 1000/limit)
-        };
+        if (limit < 1000) {
+            // logic for counter animation below 1000, displayed as animation from 0 to say 223
+            increaseCount = () => {
+                setTimeout(function () {
+                    counter.innerHTML = i.toString();
+                    if (i < limit) {
+                        i++;
+                        increaseCount();
+                    }
+                }, 1000 / limit)
+            };
+        } else if (limit >= 1000) {
+            // logic for counter animation above 1000, displayed as animation from 0.000k to say 1.320k
+            i = i / 1000;
+            const adjustedLimit = Math.floor(limit / 10) / 100;
+            increaseCount = () => {
+                setTimeout(function () {
+                    counter.innerHTML = i.toString().substr(0, 4) + "0k";
+                    if (i < adjustedLimit) {
+                        i = i + 0.01;
+                        increaseCount();
+                    }
+                }, 1000 / limit)
+            };
+        }
         increaseCount();
     };
-
 
     const init = () => {
         animateCarousel();
