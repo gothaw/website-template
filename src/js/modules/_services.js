07633c501e678f3f7801fc6724185eb14a14747e
gothaw <rad.soltan@gmail.com>
2019-12-21 14:21:49 +0100

    Services intro area finished.

--- src/js/modules/_services.js
+++ src/js/modules/_services.js
@@ -1,31 +1,28 @@
-import $ from "../lib/jquery-3.4.1.js"
-
 if (document.getElementById("services")) {
     (function () {
 
         const cards = document.querySelectorAll(".intro__card");
-        const cardFaces = document.querySelectorAll(".card__face");
 
+        /**
+         * @name            flipCardOnClick
+         * @desc            Flips services card when it is clicked. It utilizes adding and removing intro__card--flip css class.
+         */
         const flipCardOnClick = () => {
-            for(let card of cards){
+            for (let card of cards) {
                 card.addEventListener("click", function (e) {
+
                     const target = e.target;
 
                     const targetCard = target.closest(".intro__card");
 
-                    targetCard.classList.add("intro__card--flip");
-
-                    console.log(cardFaces);
-
-                    for (let cardFace of cardFaces){
-                        cards.classList.remove("card__front-face--flip");
-                        cardFace.classList.remove("card__back-face--flip");
+                    if (targetCard.classList.contains("intro__card--flip")) {
+                        targetCard.classList.remove("intro__card--flip");
+                    } else {
+                        for (let card of cards) {
+                            card.classList.remove("intro__card--flip");
+                        }
+                        targetCard.classList.add("intro__card--flip");
                     }
-
-                    targetCard.children[0].classList.add("card__front-face--flip");
-                    targetCard.children[1].classList.add("card__back-face--flip");
-                    // console.log(targetCard.children[0]);
-
                 });
             }
         };
