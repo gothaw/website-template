eac131d97d53502a103aa2a443e834c8f0aabf8a
gothaw <rad.soltan@gmail.com>
2019-12-23 20:04:31 +0100

    Changed naming convention in services intro. Added services opinion scss file.

--- src/js/modules/_services.js
+++ src/js/modules/_services.js
@@ -1,11 +1,11 @@
 if (document.getElementById("services")) {
     (function () {
 
-        const cards = document.querySelectorAll(".intro__card");
+        const cards = document.querySelectorAll(".services-intro__card");
 
         /**
          * @name            flipCardOnClick
-         * @desc            Flips services card when it is clicked. It utilizes adding and removing intro__card--flip css class.
+         * @desc            Flips services card when it is clicked. It utilizes adding and removing services-intro__card--flip css class.
          */
         const flipCardOnClick = () => {
             for (let card of cards) {
@@ -13,15 +13,15 @@ if (document.getElementById("services")) {
 
                     const target = e.target;
 
-                    const targetCard = target.closest(".intro__card");
+                    const targetCard = target.closest(".services-intro__card");
 
-                    if (targetCard.classList.contains("intro__card--flip")) {
-                        targetCard.classList.remove("intro__card--flip");
+                    if (targetCard.classList.contains("services-intro__card--flip")) {
+                        targetCard.classList.remove("services-intro__card--flip");
                     } else {
                         for (let card of cards) {
-                            card.classList.remove("intro__card--flip");
+                            card.classList.remove("services-intro__card--flip");
                         }
-                        targetCard.classList.add("intro__card--flip");
+                        targetCard.classList.add("services-intro__card--flip");
                     }
                 });
             }
